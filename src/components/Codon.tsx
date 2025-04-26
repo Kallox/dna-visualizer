104a2d2cc6c10b51e23be6a11f0d2f457c03292d
Kallox <d34d.mc@gmail.com>
2025-04-26 20:15:51 -0300

    Feat[Codon]: Special colors for Start and End

--- src/components/Codon.tsx
+++ src/components/Codon.tsx
@@ -8,8 +8,16 @@ type Props = {
 const Codon = ({ bases, position }: Props) => {
     const aminoAcid = translateToAminoAcids(bases);
 
+    const specialCodonColors: { [key: string]: string } = {
+        Methionine: "text-green-500",
+        Stop: "text-red-500",
+    };
+
     return (
-        <div className="flex flex-col font-mono" title={aminoAcid}>
+        <div
+            className={`flex flex-col font-mono ${specialCodonColors[aminoAcid]}`}
+            title={aminoAcid}
+        >
             <span className="text-xs text-gray-600">{position}</span>
             <span>{bases}</span>
         </div>
