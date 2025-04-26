cbaeb39f96801bca37dd988c411b02698f3eb4f9
Kallox <d34d.mc@gmail.com>
2025-04-26 20:04:07 -0300

    Feat[DataVisualizationZone]: Responsive

--- src/components/DataVisualizationZone.tsx
+++ src/components/DataVisualizationZone.tsx
@@ -8,11 +8,11 @@ type Props = {
 const DataVisualizationZone = ({ sequence }: Props) => {
     return (
         <div className="grid grid-cols-4 gap-2 w-full h-full">
-            <div className="col-span-3 bg-white p-8 rounded-lg shadow-md">
-                <SequenceViewer sequence={sequence} />
-            </div>
-            <div className="col-span-1 bg-white p-8 rounded-lg shadow-md">
+            <div className="order-1 lg:order-2 col-span-4 lg:col-span-1 bg-white p-8 rounded-lg shadow-md">
                 <SequenceInfo sequence={sequence} />
+            </div>
+            <div className="order-2 lg:order-1 col-span-4 lg:col-span-3 bg-white p-8 rounded-lg shadow-md">
+                <SequenceViewer sequence={sequence} />
             </div>
         </div>
     );
