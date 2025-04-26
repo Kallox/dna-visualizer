5e8b0c80571a5dee44bc9e7b9fbbd6d94efcaae2
Kallox <d34d.mc@gmail.com>
2025-04-26 00:24:09 -0300

    Feat[Codon]: Now uses font mono

--- src/components/Codon.tsx
+++ src/components/Codon.tsx
@@ -9,7 +9,7 @@ const Codon = ({ bases, position }: Props) => {
     const aminoAcid = translateToAminoAcids(bases);
 
     return (
-        <div className="flex flex-col" title={aminoAcid}>
+        <div className="flex flex-col font-mono" title={aminoAcid}>
             <span className="text-xs text-gray-600">{position}</span>
             <span>{bases}</span>
         </div>
